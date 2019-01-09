c25f212a43acceec5164b392a238ef4d49d75df1
Shamin Krook <shahmineh.gh@hotmail.com>
2019-01-09 14:24:24 +0100

    App structure

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Hello from Dashboard!
+          How are you today?
+          Weather app??? what the hell is that?
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
