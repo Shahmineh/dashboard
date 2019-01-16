c85ff16d052211041d120bd45911f9ab83f54e51
Shamin Krook <shahmineh.gh@hotmail.com>
2019-01-16 12:45:39 +0100

    Import of withRouter and MainNav in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+
 import './App.css';
+import MainNav from '../MainNav/MainNav';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>
-          Hello from Dashboard!
-          How are you today?
-          Weather app??? what the hell is that?
-        </h1>
+        <MainNav /> 
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
