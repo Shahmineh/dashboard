7d6276456bb71cd2d863c071fd9e83b5efb2ddd9
Shamin Krook <shahmineh.gh@hotmail.com>
2019-01-18 10:55:01 +0100

    Import main content component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,14 @@ import { withRouter } from 'react-router';
 
 import './App.css';
 import MainNav from '../MainNav/MainNav';
+import Main from '../Main/Main';
 
 class App extends Component {
   render() {
     return (
       <div>
         <MainNav /> 
+        <Main />
       </div>
     );
   }
---
+++ src/components/Main/Main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+
+import './Main.css';
+
+class Main extends Component {
+  render() {
+    return(
+      <div>
+        Content
+      </div>
+    );
+  }
+}
+
+export default withRouter(Main);
