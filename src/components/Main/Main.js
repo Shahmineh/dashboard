0b506d8d64de52d857234d82b056fd200bb74835
Shamin Krook <shahmineh.gh@hotmail.com>
2019-01-26 00:07:51 +0100

    Containers, rows, cols

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
+import { Container, Row, Col } from 'reactstrap';
 
+// Widgets
 import HelloWidget from'../Widgets/HelloWidget/HelloWidget';
+import Clock from '../Widgets/Clock/Clock';
 
 import './Main.scss';
 
 class Main extends Component {
   render() {
     return(
-      <div>
-        <HelloWidget />
-      </div>
+      <Container className="container">
+        <Row>
+          <Col>
+            <HelloWidget />
+          </Col>
+          <Col>
+            <Clock />
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
