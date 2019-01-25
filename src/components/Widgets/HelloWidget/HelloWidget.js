dba5a87e23d6b222ff5648c7fc71e3e2248dde0d
Shamin Krook <shahmineh.gh@hotmail.com>
2019-01-26 00:08:05 +0100

    Clock widget

--- src/components/Widgets/HelloWidget/HelloWidget.js
+++ src/components/Widgets/HelloWidget/HelloWidget.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { withRouter } from 'react-router';
 
 import './HelloWidget.scss';
@@ -6,9 +6,16 @@ import './HelloWidget.scss';
 class HelloWidget extends Component {
   render() {
     return(
-      <div>
-        Hello widget
-      </div>
+      <Fragment>
+        <div className="hello">Hello</div>
+        
+        {/* <div className="header">
+          <h2>{this.props.heading}</h2>
+        </div>
+        <div className="content">
+          {this.props.children}
+        </div> */}
+      </Fragment>
     );
 
   }
