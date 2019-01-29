2600bfd396925f76ed2719ed18ebb2be4af5ff38
Shamin Krook <shahmineh.gh@hotmail.com>
2019-01-29 22:56:27 +0100

    Import weather form

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,6 +6,11 @@ import { Container, Row, Col } from 'reactstrap';
 import Google from'../Widgets/Google/Google';
 import Youtube from '../Widgets/Youtube/Youtube';
 
+import Titles from '../Widgets/Weather/Titles';
+import Form from '../Widgets/Weather/Form';
+import Weather from '../Widgets/Weather/Weather';
+
+
 import './Main.scss';
 
 class Main extends Component {
@@ -19,7 +24,13 @@ class Main extends Component {
           <Col style={{ paddingLeft: '0', paddingRight: '0' }}>
             <Youtube />
           </Col>
-
+          <Col>
+            <div>
+              <Titles />
+              <Form />
+              <Weather />
+            </div>
+          </Col>
         </Row>
       </Container>
     );
